495526cb64dcc828143c0039304362b09d0a9235
deshankoswatte <dehami.deshan@gmail.com>
2025-07-21 14:52:13 +0530

    Add implementation to trace LDAP related methods through opentelemetry

---
+++ core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/ldap/StartTlsLdapContextFactory.java
@@ -0,0 +1,203 @@
+/*
+ * Copyright (c) 2025, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.user.core.ldap;
+
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.context.Scope;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.user.core.UserStoreException;
+import org.wso2.carbon.user.core.ldap.tracing.LDAPTracingConstants;
+import org.wso2.carbon.user.core.ldap.tracing.LDAPTracingSpan;
+import org.wso2.carbon.user.core.ldap.tracing.LdapContextTracingWrapper;
+
+import javax.naming.Context;
+import javax.naming.NamingException;
+import javax.naming.ldap.Control;
+import javax.naming.ldap.InitialLdapContext;
+import javax.naming.ldap.LdapContext;
+import javax.naming.ldap.StartTlsRequest;
+import javax.naming.ldap.StartTlsResponse;
+import java.io.IOException;
+import java.util.Hashtable;
+
+/**
+ * Factory class for initializing LDAP contexts over StartTLS, applying authentication after securing the connection,
+ * and optionally wrapping the context with tracing support. Note that most of the implementation was moved from the
+ * {@link LdapContextWrapper} class.
+ */
+public class StartTlsLdapContextFactory {
+
+    private static final Log LOG = LogFactory.getLog(StartTlsLdapContextFactory.class);
+
+    /**
+     * Initialize the LDAP context with secured connection by applying StartTLS extended operation.
+     *
+     * @param environment        environment used to create the initial Context.
+     * @param connectionControls connection request controls for the initial context.
+     * @param withTracing        if the ldap context should be wrapped with the tracing wrapper.
+     * @return secured ldap connection context.
+     * @throws NamingException    if a naming exception is encountered.
+     * @throws UserStoreException if a user store related exception is encountered.
+     */
+    public static LdapContext build(Hashtable<?, ?> environment, Control[] connectionControls, boolean withTracing)
+            throws NamingException, UserStoreException {
+
+        if (withTracing) {
+            LDAPTracingSpan ldapTracingSpan = LDAPTracingSpan.start(LDAPTracingConstants.LDAP_BUILD_START_TLS_METHOD,
+                    environment);
+            Span span = ldapTracingSpan.span();
+
+            try (Scope ignored = span.makeCurrent()) {
+                LdapContext ldapContext = createStartTlsSecuredLdapContext(environment, connectionControls, true);
+                ldapTracingSpan.success();
+                return ldapContext;
+            } catch (Exception exception) {
+                ldapTracingSpan.error(exception);
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("LDAP build start TLS failed with Exception: " + exception.getMessage(), exception);
+                }
+                throw exception;
+            }
+        } else {
+            return createStartTlsSecuredLdapContext(environment, connectionControls, false);
+        }
+    }
+
+    /**
+     * Creates a secured LDAP context by initiating a StartTLS connection and wrapping it with the appropriate context
+     * wrapper.
+     * <p>
+     * If tracing is enabled, the context is wrapped with a tracing-enabled wrapper; otherwise, a standard wrapper is used.
+     * Performs authentication if credentials are provided in the environment.
+     *
+     * @param environment        the environment properties used to create the initial context.
+     * @param connectionControls the connection controls to be used with the LDAP context.
+     * @param withTracing        whether to enable tracing on the LDAP context.
+     * @return the secured and appropriately wrapped LDAP context.
+     * @throws NamingException    if an error occurs while creating or interacting with the LDAP context.
+     * @throws UserStoreException if the StartTLS negotiation fails or authentication encounters issues.
+     */
+    private static LdapContext createStartTlsSecuredLdapContext(Hashtable<?, ?> environment,
+                                                                Control[] connectionControls, boolean withTracing)
+            throws UserStoreException, NamingException {
+
+        Hashtable<String, Object> tempEnv = StartTlsLdapContextFactory.getEnvironmentForSecuredLdapInitialization(
+                environment);
+        LdapContext ldapContext = new InitialLdapContext(tempEnv, connectionControls);
+        try {
+
+            StartTlsResponse startTlsResponse = (StartTlsResponse) ldapContext.extendedOperation(new StartTlsRequest());
+            startTlsResponse.negotiate();
+
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("StartTLS connection established successfully with LDAP server");
+            }
+
+            if (withTracing) {
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("Wrapping LDAP context with tracing-enabled context.");
+                }
+
+                LdapContextTracingWrapper ldapContextTracingWrapper = new LdapContextTracingWrapper(ldapContext,
+                        startTlsResponse);
+                performAuthenticationIfProvided(environment, ldapContextTracingWrapper.getLdapContext());
+
+                return ldapContextTracingWrapper;
+            } else {
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("Wrapping LDAP context with standard context (no tracing).");
+                }
+
+                LdapContextWrapper ldapContextWrapper = new LdapContextWrapper(ldapContext, startTlsResponse);
+                performAuthenticationIfProvided(environment, ldapContextWrapper.getLdapContext());
+
+                return ldapContextWrapper;
+            }
+        } catch (IOException e) {
+            throw new UserStoreException("Unable to establish the StartTLS connection", e);
+        }
+    }
+
+    /**
+     * Get environment variables to initialize secured LDAP context.
+     *
+     * @param environment environment used to create the initial Context.
+     * @return environment.
+     */
+    private static Hashtable<String, Object> getEnvironmentForSecuredLdapInitialization(Hashtable<?, ?> environment) {
+
+        Hashtable<String, Object> tempEnv = new Hashtable<>();
+        // Create a temp env for this particular connection by eliminating user credentials details from original env.
+        for (Object key : environment.keySet()) {
+            if (Context.SECURITY_PRINCIPAL.equals(key) || Context.SECURITY_CREDENTIALS.equals(key) ||
+                    Context.SECURITY_AUTHENTICATION.equals(key)) {
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("Attribute " + key + " is skip adding to the environment for TLS LDAP initialization");
+                }
+            } else {
+                tempEnv.put((String) key, environment.get(key));
+            }
+        }
+        return tempEnv;
+    }
+
+    /**
+     * Perform simple client authentication.
+     *
+     * @param environment environment used to create the initial Context.
+     * @param ldapContext the ldap context to which attributes should be added.
+     * @throws NamingException if a naming exception is encountered.
+     */
+    private static void performAuthenticationIfProvided(Hashtable<?, ?> environment, LdapContext ldapContext)
+            throws NamingException {
+
+        // Adding provided user credentials details one by one after TLS connection started.
+        if (environment.containsKey(Context.SECURITY_AUTHENTICATION)) {
+            ldapContext.addToEnvironment(Context.SECURITY_AUTHENTICATION,
+                    environment.get(Context.SECURITY_AUTHENTICATION));
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Attribute " + Context.SECURITY_AUTHENTICATION + " is added to the " +
+                        "TLS LdapContext environment");
+            }
+        }
+        if (environment.containsKey(Context.SECURITY_PRINCIPAL)) {
+            ldapContext.addToEnvironment(Context.SECURITY_PRINCIPAL,
+                    environment.get(Context.SECURITY_PRINCIPAL));
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Attribute " + Context.SECURITY_PRINCIPAL + " is added to the " +
+                        "TLS LdapContext environment");
+            }
+        }
+        if (environment.containsKey(Context.SECURITY_CREDENTIALS)) {
+            ldapContext.addToEnvironment(Context.SECURITY_CREDENTIALS,
+                    environment.get(Context.SECURITY_CREDENTIALS));
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Attribute " + Context.SECURITY_CREDENTIALS + " is added to the " +
+                        "TLS LdapContext environment");
+            }
+        }
+
+        /*
+        Verify whether the user is authenticated successfully.
+        {@link} https://docs.oracle.com/javase/jndi/tutorial/ldap/ext/src/StartTlsSimple.java
+         */
+        ldapContext.getAttributes("");
+    }
+}
---
+++ core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/ldap/tracing/LDAPTracingUtil.java
@@ -0,0 +1,252 @@
+/*
+ * Copyright (c) 2025, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.user.core.ldap.tracing;
+
+import io.opentelemetry.api.trace.Span;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.base.ServerConfiguration;
+
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.directory.Attribute;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.ModificationItem;
+import javax.naming.directory.SearchControls;
+import javax.naming.ldap.Control;
+import java.util.Arrays;
+
+/**
+ * Utility class that provides helper methods for OpenTelemetry-based LDAP tracing. This includes masking sensitive
+ * data, formatting control/search parameters, and reading relevant configuration values.
+ */
+public class LDAPTracingUtil {
+
+    private static final Log LOG = LogFactory.getLog(LDAPTracingUtil.class);
+
+    /**
+     * Retrieves the instrumentation scope name used for OpenTelemetry tracing.
+     * <p>
+     * This value is configured via {@code deployment.toml} and resolved using {@link ServerConfiguration} through the
+     * property key {@link LDAPTracingConstants#INSTRUMENTATION_SERVICE_NAME}. It is typically used to name the tracer
+     * (e.g., "wso2is").
+     *
+     * @return the name of the OpenTelemetry instrumentation scope.
+     */
+    public static String getInstrumentationScopeNameFromConfig() {
+
+        String scope = ServerConfiguration.getInstance().getFirstProperty(
+                LDAPTracingConstants.INSTRUMENTATION_SERVICE_NAME);
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Using instrumentation scope name: " + scope + " for OpenTelemetry tracing.");
+        }
+
+        return scope;
+    }
+
+    /**
+     * Determines whether OpenTelemetry tracing is enabled for LDAP operations.
+     * <p>
+     * This value is configured via {@code deployment.toml} and resolved using {@link ServerConfiguration} through the
+     * property key {@link LDAPTracingConstants#IS_LDAP_TRACING_ENABLED}.
+     *
+     * @return {@code true} if OpenTelemetry tracing is enabled; {@code false} otherwise.
+     */
+    public static boolean isTracingEnabled() {
+
+        boolean isEnabled = Boolean.parseBoolean(ServerConfiguration.getInstance().getFirstProperty(
+                LDAPTracingConstants.IS_LDAP_TRACING_ENABLED));
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("OpenTelemetry tracing is enabled: " + isEnabled);
+        }
+
+        return isEnabled;
+    }
+
+    /**
+     * Adds LDAP control-related metadata as span attributes.
+     * <p>
+     * Each control's OID and criticality are included in the span for visibility.
+     *
+     * @param connectionControls array of LDAP {@link Control} objects passed in the request.
+     * @param span               active OpenTelemetry span to annotate.
+     */
+    public static void addControlSpanAttributes(Control[] connectionControls, Span span) {
+
+        if (connectionControls == null || span == null) {
+            return;
+        }
+
+        for (int i = 0; i < connectionControls.length; i++) {
+            Control ctrl = connectionControls[i];
+            if (ctrl != null) {
+                span.setAttribute(LDAPTracingConstants.ATTR_LDAP_CONTROL_PREFIX + i +
+                        LDAPTracingConstants.ATTR_LDAP_CONTROL_OID_SUFFIX, ctrl.getID());
+                span.setAttribute(LDAPTracingConstants.ATTR_LDAP_CONTROL_PREFIX + i +
+                        LDAPTracingConstants.ATTR_LDAP_CONTROL_CRITICAL_SUFFIX, ctrl.isCritical());
+            }
+        }
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Added " + connectionControls.length + " LDAP control attributes to span.");
+        }
+    }
+
+    /**
+     * Masks sensitive key-value pairs in LDAP strings such as Distinguished Names (DNs) or search filters.
+     * <p>
+     * Example: {@code uid=john,ou=Users} becomes {@code uid=***,ou=***}.
+     *
+     * @param input the raw LDAP string to be masked.
+     * @return a sanitized version of the input string with attribute values replaced.
+     */
+    public static String maskLdapString(String input) {
+
+        if (StringUtils.isBlank(input)) {
+            return input;
+        }
+
+        String masked = input.replaceAll(LDAPTracingConstants.LDAP_STRING_MASK_REGEX,
+                LDAPTracingConstants.LDAP_STRING_MASK_REPLACEMENT + LDAPTracingConstants.MASKED_VALUE);
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Masked LDAP string to: " + masked);
+        }
+
+        return masked;
+    }
+
+    /**
+     * Generates a string representation of an LDAP {@link SearchControls} object for logging/tracing.
+     * <p>
+     * Captures search scope, count/time limits, and requested return attributes.
+     *
+     * @param controls the {@link SearchControls} object to format.
+     * @return formatted string of search control properties, or {@code "null"} if input is null.
+     */
+    public static String formatSearchControls(SearchControls controls) {
+
+        if (controls == null) {
+            return LDAPTracingConstants.NO_SEARCH_CONTROLS;
+        }
+
+        String formatted = String.format(
+                LDAPTracingConstants.FORMATTED_SEARCH_CONTROL_STRING,
+                controls.getSearchScope(),
+                controls.getCountLimit(),
+                controls.getTimeLimit(),
+                Arrays.toString(controls.getReturningAttributes()),
+                controls.getDerefLinkFlag(),
+                controls.getReturningObjFlag()
+        );
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Formatted search controls: " + formatted);
+        }
+
+        return formatted;
+    }
+
+    /**
+     * Masks the values of all attributes in a given {@link Attributes} object.
+     *
+     * @param attrs LDAP {@link Attributes} containing possibly sensitive data.
+     * @return a comma-separated list of attribute names with masked values.
+     */
+    public static String maskAttributes(Attributes attrs) {
+
+        StringBuilder builder = new StringBuilder();
+
+        try {
+            NamingEnumeration<? extends Attribute> allAttrs = attrs.getAll();
+            while (allAttrs.hasMore()) {
+                Attribute attr = allAttrs.next();
+                builder.append(attr.getID()).append(LDAPTracingConstants.EQUALS).append(
+                        LDAPTracingConstants.MASKED_VALUE).append(LDAPTracingConstants.DELIMITER_WITH_SPACE_SUFFIX);
+            }
+        } catch (NamingException namingException) {
+            LOG.warn("Failed to mask attributes due to LDAP exception.", namingException);
+            return LDAPTracingConstants.MASKING_ATTRIBUTE_ERROR_MESSAGE;
+        }
+
+        String maskedResult = builder.toString();
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Masked attributes: " + maskedResult);
+        }
+
+        return maskedResult;
+    }
+
+
+    /**
+     * Masks values of LDAP {@link ModificationItem}s to avoid exposing sensitive data in logs.
+     *
+     * @param mods array of {@link ModificationItem}s used in modify operations.
+     * @return string representation of attribute names with masked values.
+     */
+    public static String maskModificationItems(ModificationItem[] mods) {
+
+        StringBuilder builder = new StringBuilder();
+
+        if (mods != null) {
+            for (ModificationItem mod : mods) {
+                Attribute attr = mod.getAttribute();
+                if (attr != null) {
+                    builder.append(attr.getID()).append(LDAPTracingConstants.EQUALS).append(
+                            LDAPTracingConstants.MASKED_VALUE).append(LDAPTracingConstants.DELIMITER_WITH_SPACE_SUFFIX);
+                }
+            }
+        }
+
+        String maskedResult = builder.toString();
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Masked modification items: " + maskedResult);
+        }
+
+        return maskedResult;
+    }
+
+    /**
+     * Adds relevant attributes to the tracing span for LDAP bind or rebind operations.
+     * <p>
+     * This includes the distinguished name (DN), the class name of the bound object (if provided), and any associated
+     * attributes. Sensitive data is masked before being added to the span.
+     *
+     * @param name  the distinguished name (DN) involved in the operation.
+     * @param obj   the object being bound or rebound, may be {@code null}.
+     * @param attrs the attributes associated with the entry, may be {@code null}.
+     * @param span  the OpenTelemetry span to which the data is added.
+     */
+    public static void addBindRebindSpanData(String name, Object obj, Attributes attrs, Span span) {
+
+        if (span != null) {
+            span.setAttribute(LDAPTracingConstants.ATTR_LDAP_DN, LDAPTracingUtil.maskLdapString(name));
+            // Log the class name of the object being bound, if available.
+            if (obj != null) {
+                span.setAttribute(LDAPTracingConstants.ATTR_LDAP_OBJECT_CLASS, obj.getClass().getName());
+            }
+            if (attrs != null) {
+                span.setAttribute(LDAPTracingConstants.ATTR_LDAP_ATTRS, LDAPTracingUtil.maskAttributes(attrs));
+            }
+        }
+    }
+}
